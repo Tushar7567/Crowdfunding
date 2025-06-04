b5299184c33d591aacf5878dd0b4808fa49b611e
Tushar7567 <swain.tushar00@gmail.com>
2025-06-05 00:58:46 +0530

    intial feature complete

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,49 +1,23 @@
-require("@matterlabs/hardhat-zksync-solc");
-require("@matterlabs/hardhat-zksync-verify");
-
+require('dotenv').config();
+// require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.4.1",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zkSyncSepoliaTestnet: {
-      url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
-      zksync: true,
-      chainId: 300,
-      verifyURL:
-        "https://explorer.sepolia.era.zksync.dev/contract_verification",
-    },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      chainId: 324,
-      verifyURL:
-        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
   solidity: {
-    version: "0.8.23",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/ZhYshJRclWXKgHxJO6FV4WBullX-aJbf",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
   },
 };
